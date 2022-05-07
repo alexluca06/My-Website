5772a5faeee5c2f727748443ff67ce7ce4da3768
Alex Luca <lucaalexandru06@yahoo.com>
2022-05-07 14:49:46 +0300

    Implement Responsive NavBar

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelector('#navLinks');
+const navToggleBtn = document.querySelector('#navToggleBtn');
+
+
+
+navToggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
